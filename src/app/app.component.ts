f35cfc6e97f882a1358c93674fd3038c534e173b
MinhNq <minhnq@ftl.vn>
2021-05-24 10:35:31 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MinLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular100day';
+
+  personal_infor ={
+    name: 'Minh',
+    title: 'Information engineer',
+    company:' FTL corp'
+  };
+
+
+  handler() {
+    console.log('clicked');
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { HiComponent } from './hi/hi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    HiComponent
   ],
   imports: [
     BrowserModule
